e94b272754d70c735e2f0ebbb83d3b484a8f3672
Alejandro Vargas <61713783+AlejandroV074@users.noreply.github.com>
2022-11-01 23:10:47 -0500

    Add files via upload
    Versión final - Página SIDES

---
+++ SIDES/sides.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-10-2022 a las 02:27:59
+-- Versión del servidor: 10.3.15-MariaDB
+-- Versión de PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sides`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participantes`
+--
+
+CREATE TABLE `participantes` (
+  `DOCUMENTO` int(30) NOT NULL,
+  `NOMBRES` varchar(30) NOT NULL,
+  `APELLIDOS` varchar(30) NOT NULL,
+  `UNIVERSIDAD` varchar(40) NOT NULL,
+  `CAMPUS` varchar(30) NOT NULL,
+  `CORREO` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `participantes`
+--
+ALTER TABLE `participantes`
+  ADD PRIMARY KEY (`DOCUMENTO`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
