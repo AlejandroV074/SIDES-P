e14029869c6db9e6267f000a4026dbc62f47ab91
ederaam <117696302+ederaam@users.noreply.github.com>
2022-11-07 20:09:27 -0500

    Update conexion.php
    Se coloca conexión del servidor en la nube

--- SIDES/conexion.php
+++ SIDES/conexion.php
@@ -1,5 +1,10 @@
 <?php
 
-    $conexion = new mysqli("localhost", "root", "", "sides");
+    $conexion = new mysqli("mysql.ucc.eder.co", "sides", "UCC8382@#0asws", "sides");
      
         ?>
+
+
+//<?php
+//    $conexion = new mysqli("localhost", "root", "", "sides");
+//       ?>
