ee4145085e5f921d529f4ecfd69a2bf3641bddc6
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-04-22 18:42:53 -0500

    Activate schedule page

--- SIDES/programacion.php
+++ SIDES/programacion.php
@@ -4,7 +4,6 @@
 <head>
   <title>Agenda</title>
   <?php
-    header("location: ./index.php");
     include("./meta-tags.html");
     include("./meta-links.html");
   ?>
@@ -15,150 +14,117 @@
     include("./navbar.html");
   ?>
 
-  <div class="text-center content">
+  <div class="text-center content" id="miercoles">
     <h1 class="pre">
       Agenda
     </h1>
     <div>
       <div class="container py-2">
         <div class="column">
-          <div class="mx-auto mt-2 card">
-            <div class="card-header descanso" style="width: 100%;">
-              Jueves, 10 de noviembre de 2022
+          <div class="mx-auto mt-2 card" >
+
+            <div class="card-header fecha-evento" style="width: 100%;" >
+              Miercoles, 24 de abril de 2024
             </div>
-            <div class="card-body" style="font-size: 20px;">
-              <blockquote style="width: 100%">
+
+            <div class="card-body" style="width: 100%;">
+              <center><b style="color: #172d3b;">8:00 - 8:30</center></b>
                 
-                  <center><b style="color: #172d3b;">8:00 - 8:30</center></b>
-                
-                <b>Adriana Patricia Villegas Quiceno</b><br>
-                Universidad Cooperativa de Colombia Medellín. Decana Facultad de Ingeniería Medellín <br>
-                <b><i>Palabras de bienvenida</i></b>
-                <br>
-                <br>
-                <b>Msc. Eder Alonso Acevedo Marin</b><br>
-                Universidad Cooperativa de Colombia Medellín, Coodinador Ingeniería de Software Facultad de Ingeniería <br>
-                <b><i>Instalación del evento</i></b>
-              </blockquote>
+              <b>Adriana Patricia Villegas Quiceno</b><br>
+              Universidad Cooperativa de Colombia Medellín. Decana Facultad de Ingeniería Medellín <br>
+              <b><i>Palabras de bienvenida</i></b>
+              <br>
+              <br>
+              <b>Msc. Eder Alonso Acevedo Marin</b><br>
+              Universidad Cooperativa de Colombia Medellín. Coodinador Ingeniería de Software Facultad de Ingeniería <br>
+              <b><i>Instalación del evento</i></b>
             </div>
           </div>
-          
-          <div class="mx-auto mt-2">
-            <div class="card" style="width: 100%">
-               <div class="card-body">
-                <span class="conf"><b>CONFERENCIA </b></span><br>
-                8:30 a 9:15<br>
-                <b>PhD. Gina Paola Maestre</b><br>
-                Universidad Cooperativa de Colombia<br>
-                <b><i>Las ciudades inteligentes desde la gestión de Tecnologías de Información</i></b><br>            
-              </div>
-            </div>
-          </div>
+
           <div class="mx-auto mt-2">
             <div class="card" style="width: 100%">
               <div class="card-body">
                 <span class="conf"><b>CONFERENCIA</b></span><br>
-                9:15 a 10:00 <br>
-                <b>Msc. Lina María Montoya Suarez - Valentina Alvarez Muñoz</b><br>
-                Universidad Católica Luis Amigó<br>
-                <b><i> Minería de datos aplicado a la Industria del Software</i></b><br>
+                8:30 a 9:30<br>
+                <b>Jose Castro</b><br>
+                Soluciones de Software INDTECH SAS<br>
+                <b><i>Ciberseguridad En casa, en la U y en la Oficina</i></b><br>            
               </div>
             </div>
           </div>
-          <div class="alert mx-auto mt-2 descanso" role="alert" style="font-size: 18px; width: 100%;">
-            10:00 a 10:15 RECESO  (Coffee break)
+
+          <div class="mx-auto mt-2">
+            <div class="card" style="width: 100%">
+              <div class="card-body">
+                <span class="conf"><b>CONFERENCIA</b></span><br>
+                9:30 a 10:30<br>
+                <b>Sara</b><br>
+                México<br>
+                <b><i>Por Definir</i></b><br>
+              </div>
+            </div>
           </div>
+
+          <div class="alert mx-auto mt-2 descanso" role="alert">
+            10:30 a 11:30 RECESO  (Coffee break)
+          </div>
+
+          <div class="mx-auto mt-n2" id="jueves">
+            <div class="card" style="width: 100%">
+              <div class="card-body">
+                <span class="conf"><b>CONFERENCIA </b></span><br>
+                11:30 a 12:30 <br>
+                <b>Pedro Alberti</b><br>
+                Chile<br>
+                <b><i>Por Definir</i></b><br>            
+              </div>
+            </div>
+          </div>
+
+          <div class="mx-auto mt-2 card" >
+            <div class="card-header fecha-evento" style="width: 100%;">
+                Jueves, 25 de abril de 2024
+            </div>
+
+            <div class="card-body">
+              <span class="conf"><b>CONFERENCIA </b></span><br>
+              8:30 a 9:30 <br>
+              <b>Ronald Romani</b><br>
+              Perú<br>
+              <b><i>Por Definir</i></b><br>            
+            </div>
+          </div>
+
+          <div class="mx-auto mt-2">
+            <div class="card" style="width: 100%">
+              <div class="card-body">
+                <span class="conf"><b>CONFERENCIA</b></span><br>
+                9:30 a 10:30 <br>
+                <b>Sebastian Correa</b><br>
+                Colombia<br>
+                <b><i>Por Definir</i></b><br>            
+              </div>
+            </div>
+          </div>
+
+          <div class="alert mx-auto mt-2 descanso" role="alert">
+            10:30 a 11:30 RECESO  (Coffee break)
+          </div>
+          
           <div class="mx-auto mt-n2">
             <div class="card" style="width: 100%">
               <div class="card-body">
                 <span class="conf"><b>CONFERENCIA </b></span><br>
-                10:15 a 11:00 <br>
-                <b>Msc. Jonathan Sánchez Giraldo</b><br>
-                COMFAMA <br>
-                <b><i>Herramientas de Software libre – un aliado a la gestión TI</i></b><br>            
+                11:30 a 12:30 <br>
+                <b>Por definir</b><br>
+                N/A<br>
+                <b><i>Por definir</i></b><br>            
               </div>
-            </div>
-          </div>
-          <div class="mx-auto mt-2">
-            <div class="card" style="width: 100%">
-              <div class="card-body">
-                <span class="conf"><b>CONFERENCIA </b></span><br>
-                11:00 a 11:45 <br>
-                <b>Msc. Mauricio Betancourt</b><br>
-                INDIE LEVEL<br>
-                <b><i>¿Cómo evaluar el código fuente de otra persona?</i></b><br>            
-              </div>
-            </div>
-          </div>
-          <div class="mx-auto mt-2">
-            <div class="card" style="width: 100%">
-              <div class="card-body">
-                <span class="conf"><b>CONFERENCIA</b></span><br>
-                11:45 a 12:30 <br>
-                <b>Juan José Arroyave Serna</b><br>
-                DNA Universe<br>
-                <b><i>Web 3.0: Economía digital y NFTs</i></b><br>            
-              </div>
-            </div>
-          </div>
-          <div class="alert mx-auto mt-2 descanso" role="alert" style="font-size: 18px; width: 100%;">
-            12:30 a 14:00 RECESO - ALMUERZO
-          </div>
-          
-          <div class="mx-auto mt-n2">
-            <div class="card" style="width: 100%">
-              <div class="card-body">
-                <span class="conf"><b>CONFERENCIA </b></span><br>
-                14:00 a 14:45 <br>
-                <b>Ing. Christian Camilo Gomez </b><br>
-                 Ternium (México) <br>
-                <b><i>El mundo de los datos</i></b><br>            
-              </div>
-            </div>
-          </div>
-          <div class="mx-auto mt-2">
-            <div class="card" style="width: 100%">
-              <div class="card-body">
-                <span class="conf"><b>CONFERENCIA </b></span><br>
-                14:45 a 15:30 <br>
-                <b>por definir</b><br>
-                Perficient<br>
-                <b><i>por definir</i></b><br>            
-              </div>
+              <div class="card-header fecha-evento" style="width: 100%;"></div>
             </div>
           </div>
           
-           <div class="mx-auto mt-2">
-            <div class="card" style="width: 100%">
-              <div class="card-body">
-                <span class="conf"><b>CONFERENCIA</b></span><br>
-                
-                
-                 15:30 a 16:15 <br>
-               
-  <b>
- Marieth Baquero<br>
-Luis Angel GuarÃ­n<br>
-Salvador Villar<br>
-       </b> Semillero BERSTIC<br> 
-                <b><i>Aplicación de la realidad virtual para el desarrollo de proyectos de Ciencia, Tecnología e Innovación.</i></b><br>            
-              </div>
-            </div>
-          </div>
-          
-          <div class="mx-auto mt-2">
-            <div class="card" style="width: 100%">
-              <div class="card-body">
-                <span class="conf"><b>CONFERENCIA</b></span><br>
-                16:15 a 17:00 <br>
-                <b>Esp. Duvan Alexis Acevedo</b><br>
-                Naikka<br>
-                <b><i>¿Hola mundo? soluciona tus dudas sobre el ecosistema del software. Trae preguntas  sobre mundo laboral, programación y lenguajes, metodologías</i></b><br>            
-              </div>
-            </div>
-          </div>
-          
-          <div class="mx-auto mt-2">
+          <!-- <div class="mx-auto mt-2">
             <div class="card" style="width: 100%">
               <div class="card-body">
                 17:00 a 17:15<br>
@@ -168,10 +134,19 @@ Salvador Villar<br>
               </div>
               <div class="card-header descanso" style="width: 100%;"></div>
             </div>
-          </div>
+          </div> -->
+          
         </div>
       </div>
     </div>
+  </div>
+  <div class="position-fixed container-navigator-dates">
+    <a href="#miercoles">
+      <button type="button" class="btn btn-info btn-navigator">24</button>
+    </a>
+    <a href="#jueves">
+      <button type="button" class="btn btn-info btn-navigator">25</button>
+    </a>
   </div>
   <?php
     include("./footer.html");
