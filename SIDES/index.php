69d69c3b0af3e725f597c62d9fd83172f3889eb8
victor49 <vl491249@gmail.com>
2024-04-22 18:37:49 -0500

    correccion texto de presentacion

--- SIDES/index.php
+++ SIDES/index.php
@@ -34,9 +34,9 @@
       </h2>
       <br />
       <p align=justify style="line-height: 120%">
-        Bienvenidos a SIDES (Seminario de Ingeniería y Desarrollo de Software), el primer evento organizado por estudiantes y la coordinación de Ingeniería de software de la Facultad de ingeniería de la Universidad Cooperativa de Colombia Seccional Medellín.
+        Bienvenidos a SIDES (Seminario de Ingeniería y Desarrollo de Software), el segundo evento organizado por estudiantes y la coordinación de Ingeniería de software de la Facultad de ingeniería de la Universidad Cooperativa de Colombia Seccional Medellín.
 
-SIDES presenta temas fundamentales y de total interés para las estudiantes, profesionales y comunidad en general, interesadas en las actividades, 
+SIDES presenta temas fundamentales y de total interés para los estudiantes, profesionales y comunidad en general, interesadas en las actividades, 
         competencias y retos de la Ingeniería de software; sobre una mirada al futuro y las posibles áreas de especialización como:
         bases de datos, la inteligencia artificial, seguridad informática, entre otros. También se brindarán charlas con expertos en áreas
         temáticas afines a la ingeniería de software que serán como insumo para el desarrollo personal, profesional y social de los asistentes. 
