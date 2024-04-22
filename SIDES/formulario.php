c3391fc11b91f23753386ee625036778d66eebba
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-04-21 23:06:49 -0500

    Delete guardar.php. Implement submit to database in formulario.php. Implement card notification to show submit status

--- SIDES/formulario.php
+++ SIDES/formulario.php
@@ -15,12 +15,55 @@
     <?php
         include("./navbar.html");
     ?>
-    <div class="content">
+
+<div class="content">
+    <center>
+        <?php
+            error_reporting(E_ERROR | E_PARSE);
+            if(isset($_POST['submit'])){
+                try {
+                    include("conexion.php");
+
+                    $documento = $_POST['documento'];
+                    $nombres = $_POST['nombres'];
+                    $apellidos = $_POST['apellidos'];
+                    $universidad = $_POST['universidad'];
+                    $campus = $_POST['campus'];
+                    $correo = $_POST['correo'];
+            
+                    $query="INSERT INTO participantes(DOCUMENTO, NOMBRES, APELLIDOS, UNIVERSIDAD, CAMPUS, CORREO) VALUES ($documento,'$nombres','$apellidos','$universidad', '$campus', '$correo')";
+                    $resultado= $conexion->query($query);
+                    ?>
+
+                    <div class="card text-white text-center bg-success mb-3 mx-3" style="max-width: 30rem;">
+                        <div class="card-header">La Universidad Cooperativa de Colombia te informa:</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Registro Exitoso</h5>
+                            <p class="card-text">Tus datos han sido guardados correctamente en nuestra base de datos.</p>
+                        </div>
+                    </div>
+
+                    <?php
+                    $conexion->close();
+                } catch (Exception $e) {
+                    ?>
+                    <div class="card text-white text-center bg-danger mb-3 mx-3" style="max-width: 30rem;">
+                        <i class="card-header">La Universidad Cooperativa de Colombia te informa:</i>
+                        <div class="card-body">
+                            <h5 class="card-title">Tu registro no pudo ser completado</h5>
+                            <p class="card-text">Revisa bien los campos que ingresaste. Recuerda que solo se puede realizar un registro por persona</p>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+        ?>
+    </center>
         <center><h1 class="pre">
             Inscríbete y participa
         </h1></center>
 
-        <form action="guardar.php" method="POST">
+        <form action="formulario.php" method="POST">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
@@ -55,7 +98,7 @@
                             placeholder="Eje: user001@campusucc.edu.co">
                         <br>
                     </div>
-                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Enviar</button>
                 </div>
             </div>
         </form>
